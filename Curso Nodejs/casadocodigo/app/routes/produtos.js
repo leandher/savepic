22639273380e586f8bdd264cf2abfab30e9c81e3
invista <leandher_bessa@hotmail.com>
2017-11-24 13:22:13 -0300

    CRIANDO ROTINAS DE TESTES

--- Curso Nodejs/casadocodigo/app/routes/produtos.js
+++ Curso Nodejs/casadocodigo/app/routes/produtos.js
@@ -20,11 +20,27 @@ module.exports = function (app) {
     });
 
     app.get('/produtos/form', function (req, res) {
-        res.render('produtos/form');
+        res.render('produtos/form', {errosValidacao: {}, produto:{}});
     });
 
     app.post('/produtos', function (req, res) {
         var produto = req.body;
+
+        var validatorTitulo = req.assert('titulo', 'Titulo é obrigatório!').notEmpty();
+        var validatorTitulo = req.assert('preco', 'Formato inválido!').isFloat();
+
+        var erros = req.validationErrors();
+        if(erros){
+            res.format({
+                html: function(){
+                    res.status(400).render('produtos/form', {errosValidacao: erros, produto: produto});
+                },
+                json: function(){
+                    res.status(400).json(erros);
+                }
+            });
+            return;
+        }
         
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
---
+++ Curso Nodejs/casadocodigo/test/produtos.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var assert = require('assert');
+
+describe('#ProdutosController', function(){
+    it('#listagem json', function(done){
+        var config = {
+            hostname: 'localhost',
+            port:3000,
+            path:'/produtos',
+            headers: {
+                'Accept':'application/json'
+            }
+        };
+        
+        http.get(config, function(res){
+            assert.equal(res.statusCode,200);
+            assert.equal(res.headers['content-type'],'application/json; charset=utf-8');
+            done();
+        });
+    });
+});
